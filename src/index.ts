ce39c03ef4eef7c1c364da8a9faf53fd9bb6ced1
lismgmk <101820684+uladzimirLis@users.noreply.github.com>
2022-06-27 02:30:38 +0300

    add cors lib

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,14 @@
 import express from 'express';
 const app = express()
 const port = process.env.PORT || 5000
-const videos = [
-    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
-    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
-    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
-    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
-    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
-]
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(cors())
+app.post('/form', (req, res) => {
+    res.send('good')
 })
 
-app.get('/video', (req, res) => {
-    res.send(videos)
-})
-
-app.get('/video/:id', (req, res) => {
-    res.send(videos.find((el)=>el.id === +req.params.id))
-})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
